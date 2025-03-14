dc1b4e29ec3bdecef3c0dab3164b00afb2a46a3d
fabricemu <fabthemachine@gmail.com>
2025-03-15 00:15:40 +0200

    test

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("Number Guessing Game")
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GREEN = (0, 200, 0)
+GREEN = (0, 100, 0)
 RED = (200, 0, 0)
 BLUE = (0, 0, 50)
 
